ca93543715105342a11ee7aa50406617b98b29dd
Derek Reiff <d@derekr.net>
2024-07-02 10:22:00 -0700

    Wait for browser to load
    Hopefully this also shuts down the heroku instance?

--- index.js
+++ index.js
@@ -1,50 +1,70 @@
-const express = require('express');
-const path = require('path');
-const puppeteer = require('puppeteer');
-const execFile = require('child_process').execFile;
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const execFile = require("child_process").execFile;
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', async (req, res) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 600, height: 800 });
-    await page.goto(process.env.SCREENSHOT_URL || 'https://home-ui.vercel.app/');
-    // pause for 2 seconds to allow the page to load
-    await page.waitForTimeout(2000);
-    await page.screenshot({
-      path: '/tmp/screenshot.png',
-    });
+    .use(express.static(path.join(__dirname, "public")))
+    .set("views", path.join(__dirname, "views"))
+    .set("view engine", "ejs")
+    .get("/", async (req, res) => {
+        const browser = await puppeteer.launch({
+            dumpio: true,
+            headless: false,
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 600, height: 800 });
+        await page.goto(
+            process.env.SCREENSHOT_URL || "https://home-ui.vercel.app/",
+            { waitUntil: "networkidle0" }
+        );
+        await page.screenshot({
+            path: "/tmp/screenshot.png",
+            waitUntil: "networkidle0",
+        });
 
-    await browser.close();
+        await browser.close();
 
-    await convert('/tmp/screenshot.png');
-    screenshot = fs.readFileSync('/tmp/screenshot.png');
+        try {
+            await convert("/tmp/screenshot.png");
+        } catch (error) {
+            console.error(error);
+        }
+        screenshot = fs.readFileSync("/tmp/screenshot.png");
 
-    res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': screenshot.length,
-    });
-    return res.end(screenshot);
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
+        res.writeHead(200, {
+            "Content-Type": "image/png",
+            "Content-Length": screenshot.length,
+        });
+        return res.end(screenshot);
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 function convert(filename) {
-  return new Promise((resolve, reject) => {
-    const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
-    execFile('convert', args, (error, stdout, stderr) => {
-      if (error) {
-        console.error({ error, stdout, stderr });
-        reject();
-      } else {
-        resolve();
-      }
+    return new Promise((resolve, reject) => {
+        const args = [
+            filename,
+            "-gravity",
+            "center",
+            "-extent",
+            "600x800",
+            "-colorspace",
+            "gray",
+            "-depth",
+            "8",
+            filename,
+        ];
+        execFile("convert", args, (error, stdout, stderr) => {
+            if (error) {
+                console.error({ error, stdout, stderr });
+                reject();
+            } else {
+                resolve();
+            }
+        });
     });
-  });
 }
